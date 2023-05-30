0d175289e7b5d845e7ea39e6c3052091a4dfd9fe
jordanss1 <101638406+jordanss1@users.noreply.github.com>
2023-05-30 18:51:21 +0100

    Added new state for articles and button click

--- src/contexts/SearchContext.js
+++ src/contexts/SearchContext.js
@@ -1,38 +1,45 @@
 import React, { useState, useEffect, createContext } from "react";
-import { axiosSearchApi, handleSearchSubmit, axiosTopHeadlines } from "../api";
+import { handleSearchSubmit, axiosTopHeadlines } from "../api";
 
 const SearchContext = createContext();
 
+const art1 = [
+  {
+    author: "Stack Overflow",
+    publishedAt: "2023-05-18T01:21:17Z",
+    source: { Id: null, Name: "Stackoverflow.com" },
+    title: "Opening modalwith multiple select Options",
+    url: "https://stackoverflow.com/questions/76276563/opening-modalwith-multiple-select-options",
+  },
+  {
+    author: "Stack Overflow",
+    publishedAt: "2023-05-18T01:21:17Z",
+    source: { Id: null, Name: "Stackoverflow.com" },
+    title: "Opening modalwith multiple select Options",
+    url: "https://stackoverflow.com/questions/76276563/opening-modalwith-multiple-select-options",
+  },
+  {
+    author: "Stack Overflow",
+    publishedAt: "2023-05-18T01:21:17Z",
+    source: { Id: null, Name: "Stackoverflow.com" },
+    title: "Opening modalwith multiple select Options",
+    url: "https://stackoverflow.com/questions/76276563/opening-modalwith-multiple-select-options",
+  },
+];
+
 export const SearchStore = ({ children }) => {
-  const [articles, setArticles] = useState([]);
-  const [topArticles, setTopArticles] = useState([]);
+  const [fullArticles, setFullArticles] = useState(null);
+  const [topArticles, setTopArticles] = useState(null);
+  const [sliceArray, setSliceArray] = useState([0, 10]);
   const [searchTerm, setSearchTerm] = useState("");
   const [loading, setLoading] = useState(true);
-
-  const fullProviders = {
-    loading,
-    setLoading,
-    articles,
-    setArticles,
-    topArticles,
-    setTopArticles,
-    searchTerm,
-    setSearchTerm,
-    handleSearchSubmit,
-    axiosSearchApi,
-  };
 
   useEffect(() => {
     let id;
 
-    if (!articles.length) {
+    if (!fullArticles) {
       id = setTimeout(() => {
-        handleSearchSubmit(
-          "vulnerabilites",
-          setArticles,
-          axiosSearchApi,
-          setLoading
-        );
+        handleSearchSubmit("cyber attacks", setFullArticles, setLoading);
 
         axiosTopHeadlines.get("").then(({ data }) => {
           setTopArticles(data.articles);
@@ -42,6 +49,35 @@ export const SearchStore = ({ children }) => {
 
     return () => clearTimeout(id);
   }, []);
+
+  useEffect(() => {
+    setSliceArray([0, 10]);
+  }, [fullArticles]);
+
+  const handleButtonPress = (buttonType) => {
+    window.scrollTo({ top: 0 });
+
+    setSliceArray((prev) =>
+      prev.map((num) => (buttonType === "prev" ? num - 10 : num + 10))
+    );
+  };
+
+  const articles = fullArticles?.slice(...sliceArray);
+
+  const fullProviders = {
+    handleButtonPress,
+    sliceArray,
+    loading,
+    setLoading,
+    articles,
+    fullArticles,
+    setFullArticles,
+    topArticles,
+    setTopArticles,
+    searchTerm,
+    setSearchTerm,
+    handleSearchSubmit,
+  };
 
   return (
     <SearchContext.Provider value={fullProviders}>
